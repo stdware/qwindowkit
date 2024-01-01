d08030ea8f04f056aed88a4ad96dc9233d069787
Sine Striker <trueful@163.com>
2024-01-01 19:18:10 +0800

    Add license

--- examples/qml/main.cpp
+++ examples/qml/main.cpp
@@ -1,3 +1,6 @@
+// Copyright (C) 2023-2024 Stdware Collections
+// SPDX-License-Identifier: Apache-2.0
+
 #include <QtGui/QGuiApplication>
 #include <QtQml/QQmlApplicationEngine>
 
--- examples/shared/widgetframe/windowbar.h
+++ examples/shared/widgetframe/windowbar.h
@@ -1,3 +1,6 @@
+// Copyright (C) 2023-2024 Stdware Collections
+// SPDX-License-Identifier: Apache-2.0
+
 #ifndef WINDOWBAR_H
 #define WINDOWBAR_H
 
